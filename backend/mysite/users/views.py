dfe91e4f7cc0148b386cf68f3ec1c9c4e2aa4c52
leanhduy <laduy.hcmus@gmail.com>
2023-04-04 10:50:43 +0700

    WEB-7 URLs and Views 	implement urls and views for the website

--- backend/mysite/users/views.py
+++ backend/mysite/users/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from .models import Project, Profile
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, "users/index.html")
+    """A view to return the index page"""
+    profile = Profile.objects.first()
+    return render(request, "users/index.html", {"profile": profile})
+
+
+def single_project(request, id):
+    """A view to return a single project"""
+    project = Project.objects.get(id=id)
+    return render(request, "users/single_project.html", {"project": project})
+
+
+def personal_projects(request):
+    """A view to return all personal projects"""
+    projects = Project.objects.filter(project_type="P")
+    return render(request, "users/personal_projects.html", {"projects": projects})
+
+
+def commercial_projects(request):
+    """A view to return all commercial projects"""
+    projects = Project.objects.filter(project_type="C")
+    return render(request, "users/commercial_projects.html", {"projects": projects})
+
+
+def contact_me(request):
+    """A view to return the contact page"""
+    return render(request, "users/contact_me.html")
