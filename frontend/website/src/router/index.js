bb118d65171238cbf9582496274585c66d5204f3
leanhduy <laduy.hcmus@gmail.com>
2023-04-08 10:59:33 +0700

    WEB-12 implement Project pages

--- frontend/website/src/router/index.js
+++ frontend/website/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProjectsView from '../views/ProjectsView.vue'
 import AwardsView from '../views/AwardsView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
+  { path: '/projects', name: 'projects', component: ProjectsView },
   { path: '/awards', name: 'awards', component: AwardsView },
 ]
 
