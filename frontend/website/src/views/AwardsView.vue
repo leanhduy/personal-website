33d5ad497d432f7c7e867ff6dc9bea1d91b6622e
leanhduy <laduy.hcmus@gmail.com>
2023-04-08 14:04:38 +0700

    WEB-14 Implement the Awards page

--- frontend/website/src/views/AwardsView.vue
+++ frontend/website/src/views/AwardsView.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="container-fluid">All my awards</div>
+  <div class="container-fluid text-start">
+    <section class="resume-section" id="awards">
+      <div class="resume-section-content">
+        <h2 class="mb-5">Awards & Certifications</h2>
+        <ul class="fa-ul mb-0">
+          <li v-for="award in this.awards" :key="award.id">
+            <span class="fa-li"
+              ><i class="text-warning" :class="award.type === 'certificate' ? 'fas fa-certificate' : 'fas fa-award'"></i
+            ></span>
+            {{ award.name }} - {{ new Date(award.date_taken).getFullYear() }}
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import About from '../components/About.vue'
+import getAwards from '../composables/getAwards'
 export default {
   name: 'HomeView',
-  components: {
-    About,
+  setup() {
+    const { awards, error, load } = getAwards()
+    load()
+    return { awards, error }
   },
 }
 </script>
