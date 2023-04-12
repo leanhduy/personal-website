ef5efd0ca57c7a34d7b6caad2a8f7a9eb53b8fa2
leanhduy <laduy.hcmus@gmail.com>
2023-04-12 14:20:27 +0700

    WEB-20 migrate data from sqlite to postgres

--- backend/mysite/users/urls.py
+++ backend/mysite/users/urls.py
@@ -1,14 +1,35 @@
+import os
+from dotenv import load_dotenv
 from django.urls import path
 from . import views
 
+load_dotenv()
+
+BASE_API_URL = os.getenv("API_BASE_URL")
+
 urlpatterns = [
-    path("", views.index, name="homepage"),
-    path("api/v1/admin", views.get_admin, name="get-admin-profile"),
-    path("api/v1/projects", views.get_projects, name="get-all-projects"),
-    path("api/v1/tags", views.get_tags, name="get-all-tags"),
-    path("api/v1/tags/<uuid:id>", views.get_tag_by_id, name="get-tag-by-id"),
-    path("api/v1/skills", views.get_skills, name="get-all-skills"),
-    path("api/v1/experiences", views.get_experiences, name="get-experiences"),
-    path("api/v1/educations", views.get_educations, name="get-educations"),
-    path("api/v1/awards", views.get_awards, name="get-awards"),
+    path(f"{BASE_API_URL}/admin", views.get_admin, name="get-admin-profile"),
+    path(
+        f"{BASE_API_URL}/projects",
+        views.get_projects,
+        name="get-all-projects",
+    ),
+    path(f"{BASE_API_URL}/tags", views.get_tags, name="get-all-tags"),
+    path(
+        f"{BASE_API_URL}/tags/<int:id>",
+        views.get_tag_by_id,
+        name="get-tag-by-id",
+    ),
+    path(f"{BASE_API_URL}/skills", views.get_skills, name="get-all-skills"),
+    path(
+        f"{BASE_API_URL}/experiences",
+        views.get_experiences,
+        name="get-experiences",
+    ),
+    path(
+        f"{BASE_API_URL}/educations",
+        views.get_educations,
+        name="get-educations",
+    ),
+    path(f"{BASE_API_URL}/awards", views.get_awards, name="get-awards"),
 ]
