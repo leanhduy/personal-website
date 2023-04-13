dea27395f1d5bf366e21e5d6277f603a3fca5ad6
leanhduy <laduy.hcmus@gmail.com>
2023-04-14 00:13:56 +0700

    WEB-22 update wsgi.py

--- backend/mysite/mysite/wsgi.py
+++ backend/mysite/mysite/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+from dotenv import load_dotenv
 import os
+from django.conf import settings
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.base")
+load_dotenv(os.path.join(settings.BASE_DIR, ".env"))
 
 application = get_wsgi_application()
