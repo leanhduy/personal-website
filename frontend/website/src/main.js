60f1acf54e2b55ee5a086ddfe5007ee364f6bb4b
leanhduy <laduy.hcmus@gmail.com>
2024-05-04 09:46:50 +0700

    set up frontend with nextjs framework

--- frontend/website/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { inject } from '@vercel/analytics'
-
-createApp(App).use(router).mount('#app')
-inject()
